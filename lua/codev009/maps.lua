9f00c56fc8e64d89f8a90d9ec63083590ff10cdc
codev009 <lipin007@gmail.com>
2023-10-11 22:37:45 +0530

    set keymap for deleting buffer

--- lua/codev009/maps.lua
+++ lua/codev009/maps.lua
@@ -3,3 +3,4 @@ local keymap = require "vim.keymap"
 -- NeoTree open/close keymap set
 keymap.set('n', '<C-a>', '<ESC>:NeoTreeReveal<cr>')
 keymap.set('', '<C-s>', '<ESC>:NeoTreeClose<cr>')
+keymap.set('n', '<leader>d', ":bd! <CR>")
