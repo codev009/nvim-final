f1c6455136eeb8e1246b25a025f187fc40ca0c02
codev009 <lipin007@gmail.com>
2024-03-28 17:06:47 +0530

    Add ruby fmt

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -23,6 +23,7 @@ null_ls.setup {
     null_ls.builtins.formatting.gofumpt,
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.rubyfmt,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
