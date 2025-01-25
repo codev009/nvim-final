804422321bc1ac2b33684468a03b4541fe93c86b
codev009 <lipin007@gamil.com>
2025-01-25 20:27:18 +0530

    chore: update none-ls

--- lua/codev009/plugins.lua
+++ lua/codev009/plugins.lua
@@ -17,7 +17,7 @@ packer.startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp'      -- nvim-cmp source for neovim's built-in LSP
   use 'hrsh7th/nvim-cmp'          -- Completion
   use 'neovim/nvim-lspconfig'     -- LSP
-  use 'nvimtools/none-ls.nvim'    -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  use 'nvimtools/none-ls.nvim'
   use 'glepnir/lspsaga.nvim'
   use 'L3MON4D3/LuaSnip'
   use {
