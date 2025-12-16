13c59df3ecacf510033c8b34c360a08fb6cb1ca8
Lipin Pappachan <lipin007@gmail.com>
2025-12-16 22:28:30 +0530

    Update treesitter branch to main

--- lua/codev009/plugins.lua
+++ lua/codev009/plugins.lua
@@ -29,6 +29,7 @@ packer.startup(function(use)
   }
   use {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     run = function()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
